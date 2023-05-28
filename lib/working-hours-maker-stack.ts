9c306047542a0072e997a95e461f2005185821d3
akicitron308 <impeeeeedance@icloud.com>
2023-05-28 22:35:43 +0900

    first deploy

--- lib/working-hours-maker-stack.ts
+++ lib/working-hours-maker-stack.ts
@@ -1,19 +1,34 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import { FunctionUrlAuthType, LayerVersion } from 'aws-cdk-lib/aws-lambda';
+import { BlockPublicAccess } from 'aws-cdk-lib/aws-s3';
 
-export class WorkingHoursMakerStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+
+export class WorkingHoursMakerStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'WorkingHoursMakerQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const getWorkingHours = new lambda.Function(this, 'GetWorkingHours',{
+      functionName: 'get_working_hours',
+      runtime: lambda.Runtime.PYTHON_3_9,
+      code: lambda.Code.fromAsset('src/lambda'),
+      handler: 'get_working_hours.lambda_handler'
     });
+    getWorkingHours.addFunctionUrl({
+      authType: FunctionUrlAuthType.NONE,
+      cors: {
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedOrigins: ['*']
+      }
+    })
 
-    const topic = new sns.Topic(this, 'WorkingHoursMakerTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const bucket = new s3.Bucket(this, 'working-hours-bucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      versioned: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
   }
 }
---
+++ src/lambda/get_working_hours/get_working_hours.py
@@ -0,0 +1,264 @@
+import os
+import json
+import logging
+import requests
+
+import boto3
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# ロギングの初期設定
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+# 正常終了
+NORMAL_RESULT = "0000"
+# ファイル取得失敗
+FAILED_GET_FILE = "0010"
+
+s3 = boto3.client("s3")
+
+
+def lambda_handler(event: dict, context: dict) -> dict:
+    """
+    Lambdaハンドラー
+
+    param
+    -----------
+    event: dict
+    context: dict
+    """
+    body: dict = json.loads(event.get("body"))
+    logging.info(f"■body: {body}")
+
+    res: dict = logic(body)
+    return res
+
+
+def logic(body: dict) -> dict:
+    """
+    メインロジック
+
+    param
+    ------------
+    body: dict
+        SlackAPIからのリクエストボディ
+
+    return
+    ------------
+    dict
+        API Gatewayへ返すレスポンス
+    """
+    logging.debug("Start")
+
+    # SlackAPIの認証リクエストを判定
+    if check_challenge(body):
+        challenge: str = body["challenge"]
+        res: dict = create_response(NORMAL_RESULT, challenge=challenge)
+        logging.debug(f"{__name__}: End")
+
+        return res
+
+    # 環境情報を取得
+    token: str = os.environ["SLACK_ACCESS_TOKEN"]
+    s3_bucket: str = "amp-wsm-work-schedules"
+    s3_prefix: str = "raw/"
+    logging.info(
+        f"■token: {token}, ■s3_bucket: {s3_bucket}, ■s3_prefix: {s3_prefix}"
+    )
+
+    # ファイル情報を取得
+    file_id: str = body["event"]["file_id"]
+    client: WebClient = WebClient(token=token)
+    logging.info(f"■file_id: {file_id}")
+
+    # Slackからファイル情報を取得
+    file_info = get_file_info_for_slack(client, file_id, token)
+    logging.info(f"■file_info: {file_info}")
+
+    if file_info:
+        # ファイル取得
+        file_content: bytes = download_file_for_slack(file_info, token)
+        logging.info(f"■file_content: {file_content}")
+
+    # ファイル取得失敗
+    if not file_content:
+        res: dict = create_response(FAILED_GET_FILE)
+        logging.debug("End")
+        return res
+
+    # 格納するファイル名を作成
+    file_name: str = get_file_name(file_info)
+    s3_path: str = s3_prefix + file_name
+    logging.info(f"s3_path: {s3_path}")
+
+    # S3へファイルを格納
+    s3_put_file(s3, file_content, s3_bucket, s3_path)
+
+    res = create_response(NORMAL_RESULT)
+
+    logging.debug("End")
+    return res
+
+
+def check_challenge(body: dict) -> bool:
+    """
+    SlackAPIからの認証リクエストを判定
+
+    param
+    ------------
+    body: dict
+        SlackAPIからのリクエストボディ
+
+    return
+    ------------
+    eool
+        認証リクエストが送られている場合はTrue
+    """
+    logging.debug("Start")
+
+    if body["type"] == "url_verification":
+        return True
+
+    logging.debug("End")
+    return False
+
+
+def get_file_name(file_info: str) -> str:
+    """
+    SlackAPIからファイル名を取得
+
+    param
+    ---------
+    file_info: dict
+        アップロードされたファイルの情報
+
+    return
+    ---------
+    str
+        ファイル名
+    """
+    logging.debug("Start")
+
+    # ファイル名の取得
+    file_name: str = file_info["file"].get("name")
+
+    logging.debug("End")
+    return file_name
+
+
+def get_file_info_for_slack(
+    client: WebClient, file_id: str, token: str
+) -> dict:
+    """
+    Slackにアップロードされたファイルの情報を取得
+
+    param
+    ---------
+    client: WebClient
+        SlackのWebClient
+    file_id: str
+        アップロードされたファイルのID
+
+    return
+    ---------
+    None
+    """
+    logging.debug("Start")
+
+    file_info = None
+    try:
+        file_info = client.files_info(token=token, file=file_id)
+
+    except SlackApiError as err:
+        logging.info(f"Error downloading file: {err}")
+
+    logging.debug("End")
+
+    return file_info
+
+
+def download_file_for_slack(file_info: dict, token: str):
+    """
+    Slackにアップロードされたファイルを取得する
+
+    param
+    ------------
+    file_info: dict
+        Slackにアップロードされたファイル情報
+    token: str
+        Slackのアクセストークン
+    """
+    download_url: str = file_info["file"].get("url_private_download")
+    file_content = None
+    if download_url:
+        headers = {"Authorization": f"Bearer {token}"}
+        try:
+            response: requests.Response = requests.get(
+                download_url, headers=headers
+            )
+            logging.info(f"■response: {response}")
+            file_content: bytes = response.content
+            logging.info(f"■file_content: {file_content}")
+        except Exception as err:
+            logging.error(err)
+
+    return file_content
+
+
+def s3_put_file(s3_client, file: bytes, s3_bucket: str, s3_path: str) -> None:
+    """
+    S3にファイルをアップロードする
+
+    param
+    ------------
+    s3_client
+        S3のboto3クライアント
+    file: bytes
+        アップロード対象のファイル
+    s3_bucket: str
+        アップロード先のS3バケット名
+    s3_path: str
+        アップロードするS3のプレフィックス名 + ファイル名
+
+    return
+    -----------
+    None
+    """
+    logging.debug("Start")
+
+    s3_client.put_object(Bucket=s3_bucket, Body=file, Key=s3_path)
+
+    logging.debug("End")
+
+
+def create_response(result_code: str, challenge: str = None) -> dict:
+    """
+    レスポンスを作成する
+
+    param
+    ------------
+    result_code: str
+        アプリケーションの応答コード
+    challenge: str
+        Slackの認証リクエスト
+
+    return
+    ------------
+    dict
+        API Gatewayに返却するレスポンス
+    """
+    logging.debug("Start")
+
+    res: dict = {
+        "statusCode": 200,
+        "body": {
+            "result_code": result_code,
+        },
+    }
+
+    if challenge:
+        res["body"]["challenge"] = challenge
+
+    logging.debug("End")
+    return res
