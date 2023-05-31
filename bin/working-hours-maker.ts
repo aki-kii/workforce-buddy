a909161c0b798a3b298c3bbb765ac9d7c7125fa3
akicitron308 <impeeeeedance@icloud.com>
2023-06-01 00:07:45 +0900

    フォーマットの修正

--- bin/working-hours-maker.ts
+++ bin/working-hours-maker.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { WorkingHoursMakerStack } from '../lib/working-hours-maker-stack';
+import * as cdk from "aws-cdk-lib";
+import { WorkingHoursMakerStack } from "../lib/working-hours-maker-stack";
 
 const app = new cdk.App();
-new WorkingHoursMakerStack(app, 'WorkingHoursMakerStack');
+new WorkingHoursMakerStack(app, "WorkingHoursMakerStack");
